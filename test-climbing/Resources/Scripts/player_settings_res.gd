554cde6a42d7023ec445f63d38fa56934170ca19
Sytsn <jwclaus12@gmail.com>
2026-05-18 20:56:56 -0400

    remove test climbing project from the repo

--- test-climbing/Resources/Scripts/player_settings_res.gd
+++
@@ -1,5 +0,0 @@
-class_name PlayerSettingsRes extends Resource
-
-@export_category("Camera Visuals")
-@export var camera_spring_enabled: bool
-@export var camera_lean_enabled: bool
--- test-climbing/Scripts/AutoLoads/Global.gd
+++
@@ -1,3 +0,0 @@
-extends Node
-
-@onready var player_settings_res: PlayerSettingsRes = preload("res://Resources/Scenes/Player/player_settings.tres")
